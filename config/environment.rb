b37b560df68b9cbc4e489cab64c5de8a86c50a07
nerangel <quentindtouroul@gmail.com>
2020-11-04 08:22:57 +0100

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => 'https://fierce-caverns-96407.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
