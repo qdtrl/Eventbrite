a19af41d056c1a150fac06ac236a8cbffaa45a93
nerangel <quentindtouroul@gmail.com>
2020-11-04 08:14:04 +0100

    bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,22 @@ class UsersController < ApplicationController
     @events = Event.all
   end
 
+  def edit
+    
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def only_see_own_page
@@ -15,5 +31,9 @@ class UsersController < ApplicationController
       if current_user != @user
         redirect_to root_path, notice: "Sorry, but you are only allowed to view your own profile page."
       end
+  end
+
+  def event_params
+    params.require(:event).permit(:last_name, :first_name, :description)
   end
 end
